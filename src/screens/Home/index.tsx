e24ee0f4a2627335fcce2252e16a30e99b00d172
Kavin Phan <kavin@kphan.tech>
2022-01-08 23:07:00 -0500

    Update index with notification button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,31 @@
 import type { NextPage } from "next";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  const sendNotification = () => {
+    if (Notification.permission === "granted") {
+      navigator.serviceWorker.getRegistration().then((reg) => {
+        reg?.showNotification("Hello world!");
+      });
+    }
+  };
+
+  // Request notification permission immediately
+  useEffect(() => {
+    Notification.requestPermission();
+  }, []);
+
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <p className="text-4xl font-bold">Hello world!</p>
+    <div className="w-screen h-screen flex flex-col justify-center items-center space-y-8">
+      <p className="text-4xl font-bold drop-shadow-lg">
+        Next.js + TypeScript + TailwindCSS PWA
+      </p>
+      <button
+        className="bg-gradient-to-r from-cyan-400 to-blue-400 text-white px-4 py-2 rounded-md text-2xl drop-shadow-md hover:drop-shadow-lg transition-all"
+        onClick={sendNotification}
+      >
+        Click Me!
+      </button>
     </div>
   );
 };
