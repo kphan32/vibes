51efd034e7b7139fcef416709f490c833fd0dc6f
Kavin Phan <kavin@kphan.tech>
2022-02-09 02:12:52 -0500

    Refactor vibe check entry into its own component

--- src/screens/VibeHistory/index.tsx
+++ src/screens/VibeHistory/index.tsx
@@ -1,10 +1,56 @@
 import clsx from "clsx";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { FC, useState } from "react";
 import { HiOutlineTrash } from "react-icons/hi";
 import useVibeChecks from "../../hooks/useVibeChecks";
 import VibeCheck from "../../types/vibeCheck";
+
+interface VibeCheckEntryProps {
+  key: number;
+  vibeCheck: VibeCheck;
+  deleted: VibeCheck[];
+  setToDelete: (vibeCheck: VibeCheck) => void;
+}
+
+const VibeCheckEntry: FC<VibeCheckEntryProps> = ({
+  key,
+  vibeCheck,
+  deleted,
+  setToDelete,
+}) => {
+  const timestamp = new Date(vibeCheck.timestamp);
+
+  return (
+    <div
+      className={clsx(
+        `flex flex-row justify-between
+               w-72 m-4 p-3 rounded-md bg-gray-100 drop-shadow-md
+               transition-opacity duration-150`,
+        { "opacity-0": deleted.includes(vibeCheck) }
+      )}
+      key={key}
+    >
+      <div>
+        <p className={clsx(`text-sm text-gray-500`)}>
+          {timestamp.toLocaleString("en-US", {
+            timeStyle: "short",
+            dateStyle: "short",
+          })}
+        </p>
+        <p>{vibeCheck.description}</p>
+      </div>
+      <div className="flex items-center">
+        <HiOutlineTrash
+          className={clsx(
+            `w-6 h-6 cursor-pointer text-gray-400 hover:text-red-500 transition-colors`
+          )}
+          onClick={() => setToDelete(vibeCheck)}
+        />
+      </div>
+    </div>
+  );
+};
 
 const VibeChecks = () => {
   const router = useRouter();
@@ -32,39 +78,14 @@ const VibeChecks = () => {
         </p>
       </div>
       <div className="mt-8">
-        {vibeChecks.map((vibeCheck, i) => {
-          const timestamp = new Date(vibeCheck.timestamp);
-
-          return (
-            <div
-              className={clsx(
-                `flex flex-row justify-between
-               w-72 m-4 p-3 rounded-md bg-gray-100 drop-shadow-md
-               transition-opacity duration-150`,
-                { "opacity-0": deleted.includes(vibeCheck) }
-              )}
-              key={i}
-            >
-              <div>
-                <p className={clsx(`text-sm text-gray-500`)}>
-                  {timestamp.toLocaleString("en-US", {
-                    timeStyle: "short",
-                    dateStyle: "short",
-                  })}
-                </p>
-                <p>{vibeCheck.description}</p>
-              </div>
-              <div className="flex items-center">
-                <HiOutlineTrash
-                  className={clsx(
-                    `w-6 h-6 cursor-pointer text-gray-400 hover:text-red-500 transition-colors`
-                  )}
-                  onClick={() => setToDelete(vibeCheck)}
-                />
-              </div>
-            </div>
-          );
-        })}
+        {vibeChecks.map((vibeCheck, i) => (
+          <VibeCheckEntry
+            key={i}
+            vibeCheck={vibeCheck}
+            deleted={deleted}
+            setToDelete={setToDelete}
+          />
+        ))}
       </div>{" "}
     </div>
   );
