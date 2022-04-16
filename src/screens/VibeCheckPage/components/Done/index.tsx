7e48e4ebe1d84bd69025697a5062759625533d14
Kavin Phan <kavin@kphan.tech>
2022-04-15 20:42:27 -0400

    Refactor done component to use styled components

--- src/screens/VibeCheckPage/components/Done/index.tsx
+++ src/screens/VibeCheckPage/components/Done/index.tsx
@@ -1,23 +1,52 @@
-import clsx from "clsx";
 import { FC } from "react";
 import { HiOutlineCheckCircle } from "react-icons/hi";
+import tw from "tailwind-styled-components";
+
+const Done: FC<DoneProps> = ({ submitted }) => {
+  return (
+    <Container submitted={submitted}>
+      <Text>Done</Text>
+      <DoneIcon />
+    </Container>
+  );
+};
+
+const Container = tw.div`
+  ${(props: DoneProps) => (props.submitted ? "opacity-100" : "opacity-0")}
+
+  absolute
+  left-0
+  top-0
+
+  w-screen
+  h-screen
+  
+  flex
+  flex-col
+  justify-center
+  items-center 
+  space-y-4
+  
+  pointer-events-none
+  
+  transition-all
+  duration-250
+`;
+
+const Text = tw.div`
+  text-4xl
+  font-semibold
+`;
+
+const DoneIcon = tw(HiOutlineCheckCircle)`
+  w-24
+  h-24
+
+  text-green-500
+`;
 
 interface DoneProps {
   submitted: boolean;
 }
-
-const Done: FC<DoneProps> = ({ submitted }) => {
-  return (
-    <div
-      className={clsx(
-        "w-screen h-screen absolute left-0 top-0 flex flex-col justify-center items-center space-y-4 pointer-events-none transition-all duration-250",
-        { "opacity-0": !submitted, "opacity-100": submitted }
-      )}
-    >
-      <p className="text-4xl font-semibold">Done</p>
-      <HiOutlineCheckCircle className="w-24 h-24 text-green-500" />
-    </div>
-  );
-};
 
 export default Done;
