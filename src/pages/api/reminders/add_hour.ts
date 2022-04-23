10cb06d132bd8a4042bd10d429177f13d01910c8
Kavin Phan <kavin@kphan.tech>
2022-04-23 00:34:29 -0400

    Add add_hour endpoint

---
+++ src/pages/api/reminders/add_hour.ts
@@ -0,0 +1,43 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prismaClient from "../../../db";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const body: PutAddHourRequestBody = JSON.parse(req.body);
+
+    try {
+      await prismaClient.reminderTime.upsert({
+        where: {
+          subscriptionEndpoint_hour: {
+            subscriptionEndpoint: body.endpoint,
+            hour: body.hour,
+          },
+        },
+        update: {},
+        create: {
+          subscriptionEndpoint: body.endpoint,
+          hour: body.hour,
+        },
+      });
+
+      return res.status(200).end();
+    } catch (e) {
+      console.error(
+        `Error adding reminder time at hour ${body.hour} for endpoint "${body.endpoint}`
+      );
+
+      console.error(e);
+
+      return res.status(500).end();
+    }
+  }
+
+  return res.status(405).end();
+};
+
+export interface PutAddHourRequestBody {
+  endpoint: string;
+  hour: number;
+}
+
+export default handler;
